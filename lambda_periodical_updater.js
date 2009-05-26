0e25a14bff6be90369b331366e959f6e7f3b6fca
Giles Bowkett <gilesb@gmail.com>
2009-05-26 16:19:49 -0700

    init

---
+++ lambda_periodical_updater.js
@@ -0,0 +1,44 @@
+// this is a copy of PeriodicalUpdater which only changes one thing - it takes a function for its URL arg,
+// rather than a literal value, and so can call different URLs at different times.
+
+Ajax.LambdaPeriodicalUpdater = Class.create(Ajax.Base, {
+  initialize: function($super, container, url_function, options) {
+    $super(options);
+    this.onComplete = this.options.onComplete;
+
+    this.frequency = (this.options.frequency || 2);
+    this.decay = (this.options.decay || 1);
+
+    this.updater = { }; 
+    this.container = container;
+    this.url_function = url_function; 
+
+    this.start();
+  },
+
+  start: function() {
+    this.options.onComplete = this.updateComplete.bind(this);
+    this.onTimerEvent();
+  },
+
+  stop: function() {
+    this.updater.options.onComplete = undefined;
+    clearTimeout(this.timer);
+    (this.onComplete || Prototype.emptyFunction).apply(this, arguments);
+  },
+
+  updateComplete: function(response) {
+    if (this.options.decay) {
+      this.decay = (response.responseText == this.lastText ?
+        this.decay * this.options.decay : 1);
+
+      this.lastText = response.responseText;
+    }    
+    this.timer = this.onTimerEvent.bind(this).delay(this.decay * this.frequency);
+  },
+
+  onTimerEvent: function() {
+    this.updater = new Ajax.Updater(this.container, this.url_function(), this.options);
+  }
+});
+
