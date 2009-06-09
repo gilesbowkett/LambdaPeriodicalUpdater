052c6c2d8e3040e872b9aad299ca7fa7d6d99b88
Giles Bowkett <gilesb@gmail.com>
2009-06-08 23:53:01 -0700

    got a clue. threw out copy-paste, used OOP (gasp what an idea zomg)

--- lambda_periodical_updater.js
+++ lambda_periodical_updater.js
@@ -1,44 +1,15 @@
 // this is a copy of PeriodicalUpdater which only changes one thing - it takes a function for its URL arg,
 // rather than a literal value, and so can call different URLs at different times.
 
-Ajax.LambdaPeriodicalUpdater = Class.create(Ajax.Base, {
+// http://gilesbowkett.blogspot.com/2009/05/lambdaperiodicalupdater.html
+
+Ajax.LambdaPeriodicalUpdater = Class.create(Ajax.PeriodicalUpdater, {
   initialize: function($super, container, url_function, options) {
-    $super(options);
-    this.onComplete = this.options.onComplete;
-
-    this.frequency = (this.options.frequency || 2);
-    this.decay = (this.options.decay || 1);
-
-    this.updater = { }; 
-    this.container = container;
-    this.url_function = url_function; 
-
-    this.start();
-  },
-
-  start: function() {
-    this.options.onComplete = this.updateComplete.bind(this);
-    this.onTimerEvent();
-  },
-
-  stop: function() {
-    this.updater.options.onComplete = undefined;
-    clearTimeout(this.timer);
-    (this.onComplete || Prototype.emptyFunction).apply(this, arguments);
-  },
-
-  updateComplete: function(response) {
-    if (this.options.decay) {
-      this.decay = (response.responseText == this.lastText ?
-        this.decay * this.options.decay : 1);
-
-      this.lastText = response.responseText;
-    }    
-    this.timer = this.onTimerEvent.bind(this).delay(this.decay * this.frequency);
+    $super(container, url_function, options);
   },
 
   onTimerEvent: function() {
-    this.updater = new Ajax.Updater(this.container, this.url_function(), this.options);
+    this.updater = new Ajax.Appender(this.container, this.url(), this.options);
   }
 });
 
